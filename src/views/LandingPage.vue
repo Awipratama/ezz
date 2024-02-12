0541543b6886ac84e30ac7c6c177a7357ca7007c
Awipratama <awipratama05122006@gmail.com>
2024-02-12 15:25:23 +0800

    Booking Place

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,9 +1,12 @@
 <script>
 import MainNavigation from '@/components/MainNavigation.vue';
 import masonry from 'masonry-layout';
+import MainFooter from '@/components/MainFooter.vue';
+
 export default {
   components: {
-    Navigation: MainNavigation
+    Navigation: MainNavigation,
+    MainFooter: MainFooter
   },
   data() {
     return {
@@ -231,14 +234,14 @@ export default {
   </section>
   <section class="booking">
     <div class="container">
-      <div class="custom-card w-100">
+      <div class="custom-card shadow w-100">
         <div class="row justify-content-around">
           <div class="col-2">
-            <h5>Date</h5>
+            <h5 class="input-text">Date</h5>
             <input type="datetime-local" name="Date" placeholder="Date" class="booking-input" />
           </div>
           <div class="col-2">
-            <h5>Check-in</h5>
+            <h5 class="input-text">Check-in</h5>
             <input
               type="datetime-local"
               name="Checkin"
@@ -247,7 +250,7 @@ export default {
             />
           </div>
           <div class="col-2">
-            <h5>Check-out</h5>
+            <h5 class="input-text">Check-out</h5>
             <input
               type="datetime-local"
               name="Checkout"
@@ -256,7 +259,7 @@ export default {
             />
           </div>
           <div class="col-2">
-            <h5>Promo Code</h5>
+            <h5 class="input-text">Promo Code</h5>
             <input type="number" name="Name" placeholder="*Promo Code" class="booking-input" />
           </div>
           <div class="col-2">
@@ -266,6 +269,7 @@ export default {
       </div>
     </div>
   </section>
+  <MainFooter />
 </template>
 
 <style scoped>
@@ -568,25 +572,32 @@ export default {
 .custom-card {
   padding-left: 50px;
 }
-.booking .container {
-  padding: 50px;
+.custom-card {
+  padding: 30px;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  /* box-shadow: 1px 3px 0 3px gray; */
 }
 .booking-input {
   padding: 8px;
   border-radius: 8px;
 }
+.input-text {
+  font-weight: 400;
+  font-size: 16px;
+}
 .booking-button {
   width: 100%;
   padding: 8px;
-  background: white;
-  color: black;
+  background: black;
+  color: white;
   border-radius: 8px;
-  margin-top: 32px;
+  margin-top: 27px;
   transition: all 500ms;
 }
 .booking-button:hover {
-  background: black;
-  color: white;
+  background: white;
+  color: black;
 }
 @media only screen and (min-width: 500px) {
   .hero-text-content {

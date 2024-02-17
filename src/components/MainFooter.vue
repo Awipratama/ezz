addaefe6c56c784c9d7e0f7864179b454455c8a9
Awipratama <awipratama05122006@gmail.com>
2024-02-17 12:07:29 +0800

    gabut

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -3,9 +3,9 @@ export default {
   data() {
     return {
       yearNow: new Date().getFullYear()
-    }
+    };
   }
-}
+};
 </script>
 <template>
   <footer class="footer">
@@ -40,8 +40,14 @@ export default {
               <h2 class="text-white"><i class="bi bi-geo-alt pe-2 py-1"></i>Maps</h2>
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d504870.73627706285!2d114.6302318572998!3d-8.662742094336052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd24063dbb521f7%3A0xf841c6383a113418!2sElizabeth%20International%20Denpasar%20-%20Ezzy%20City!5e0!3m2!1sid!2sid!4v1707555892777!5m2!1sid!2sid"
-                width="500" height="300" style="border: 0" async allowfullscreen loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                width="500"
+                height="300"
+                style="border: 0"
+                async
+                allowfullscreen
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
           </div>
         </div>
@@ -49,17 +55,22 @@ export default {
     </div>
     <div class="sec-footer bg-green">
       <div class="container-fluid ps-5">
-        <div class="row ">
+        <div class="row">
           <div
-            class="social-icon mb-3 mb-lg-0 col-lg-4 col-12 d-flex flex-wrap justify-content-center justify-content-lg-start">
+            class="social-icon mb-3 mb-lg-0 col-lg-4 col-12 d-flex flex-wrap justify-content-center justify-content-lg-start"
+          >
             <div class="fb">
-              <a href="/facebook.com"><i class="bi bi-facebook fs-4"></i></a>
+              <a href="/facebook.com" target="_blank"><i class="bi bi-facebook fs-4"></i></a>
             </div>
             <div class="int">
-              <a href="http://instagram.com/ezzyhotel"><i class="int bi bi-instagram fs-4"></i></a>
+              <a href="http://instagram.com/ezzyhotel" target="_blank"
+                ><i class="int bi bi-instagram fs-4"></i
+              ></a>
             </div>
             <div class="yt">
-              <a href="https://www.youtube.com/results?search_query=ezzy+tv"><i class="yt bi bi-youtube fs-4"></i></a>
+              <a href="https://www.youtube.com/results?search_query=ezzy+tv" target="_blank"
+                ><i class="yt bi bi-youtube fs-4"></i
+              ></a>
             </div>
           </div>
           <div class="col-lg-7 col-12">
@@ -88,7 +99,7 @@ ul li {
   background-color: #222222;
 }
 
-.footer>div {
+.footer > div {
   /* max-width: 1200px; */
   width: 100%;
   margin-inline: auto;
